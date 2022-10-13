cfb9d8f93622a0de39077326de03a2bdb64d4415
Muibaby1 <oyeniyimuinat11@gmail.com>
2022-10-13 10:50:47 -0700

    task 5

---
+++ 0x00-hello_world/5-print.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+
+int main(void)
+{
+		/*
+		 * 	 * function to output a text string using 
+		 * 	 	 * printf function.
+		 * 	 	 	 *
+		 * 	 	 	 	 */
+		printf("with proper grammar, but the outcome is a piece of art,");
+			return (0);
+}
