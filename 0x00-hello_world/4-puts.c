d12317ecdae7cec5712716d0a66680ef8cab7958
Muibaby1 <oyeniyimuinat11@gmail.com>
2022-10-13 10:44:23 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
+/**
+ *  *main - Entry point
+ *   *
+ *    *Return: Always 0 (Success)
+ *     */
 int main(void)
 {
-		/**
-		 * 	 *A program to output a string using puts function.
-		 * 	 	 */
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
